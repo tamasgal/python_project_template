db311b9b4216b88e10610f17b96008f7143518ab
Tamas Gal <tgal@km3net.de>
2019-01-22 14:18:47 +0100

    Cleanup and add test facility

---
+++ foo/bar.py
@@ -0,0 +1,33 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: bar.py
+"""
+The bar module.
+
+"""
+from __future__ import absolute_import, print_function, division
+
+__author__ = "Your Name"
+__credits__ = ["The guy in your office", "Tom"]
+__license__ = "MIT"
+__maintainer__ = "Your Buddy"
+__email__ = "yname@km3net.de"
+
+
+def whats_the_meaning_of_life(n_cores=23):
+    """Answers the question about the meaning of life.
+
+    You don't even have to ask the question, it will figure it out for you.
+    Don't use more cores than available to mankind.
+    
+    Parameters
+    ----------
+    n_cores: int [default: 23]
+        The number of CPU cores to use.
+
+    Returns
+    -------
+    int
+        The type of the expected answer is of course an integer.
+    """
+    return 42
---
+++ foo/tests/test_bar.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: test_bar.py
+from unittest import TestCase
+
+from foo.bar import whats_the_meaning_of_life
+
+__author__ = "Your Name"
+__credits__ = []
+__license__ = "MIT"
+__maintainer__ = "Your Name"
+__email__ = "yname@km3net.de"
+
+
+class TestMeaningOfLife(TestCase):
+    """Tests for the bar module"""
+
+    def test_meaning_of_life(self):
+        assert 1 == whats_the_meaning_of_life()
+
+    def test_meaning_of_life_with_one_core(self):
+        assert 1 == whats_the_meaning_of_life(n_cores=1)
